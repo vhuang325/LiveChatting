155e93d5d051bdc34940cfba348935fccfd777fd
vhuang325 <vhuang325@gmail.com>
2023-07-28 16:08:26 -0400

    added some more css and scroll function to work

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const chatBottomRef = useRef(null);
+
   const sendMessage = async () => {
     if (currentMessage !== "") {
       const messageData = {
         room: room,
-        author: username,
+        owner: username,
         message: currentMessage,
         time:
           new Date(Date.now()).getHours() +
@@ -17,6 +19,7 @@ function Chat({ socket, username, room }) {
 
       await socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
@@ -27,34 +30,43 @@ function Chat({ socket, username, room }) {
     });
   }, [socket]);
 
+  //scroll to bottom
+  useEffect(() => {
+    chatBottomRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messageList]);
+
   return (
     <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
       <div className="chat-body">
-        {messageList.map((messageContent) => {
-          return (
-            <div
-              className="message"
-              id={username === messageContent.author ? "you" : "other"}
-            >
-              <div>
-                <div className="message-content">
-                  <p>{messageContent.message}</p>
-                </div>
-                <div className="message-meta">
-                  <p id="time">{messageContent.time}</p>
-                  <p id="author">{messageContent.author}</p>
+        <div className="message-container" ref={chatBottomRef}>
+          {messageList.map((messageContent, index) => {
+            return (
+              <div
+                key={index}
+                className="message"
+                id={username === messageContent.owner ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="owner">{messageContent.owner}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
       <div className="chat-footer">
         <input
           type="text"
+          value={currentMessage}
           placeholder="Hey..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
